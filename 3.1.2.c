aa08f7e9bf31acdd539f77fd6d1e4cd7e5f8dfe1
Fathin <dinamaidien1973@gmail.com>
2022-06-22 22:51:52 +0800

    commit for newest lab 3 exercise

--- 3.1.2.c
+++ 3.1.2.c
@@ -10,7 +10,7 @@ int main(void)
 
 void sigint_handler(int sig);
 void sigtstp_handler(int sigt);
-void sigquit_handler (int sigq)
+void sigquit_handler (int sigq);
 char s[200];
 
 if (signal(SIGINT,sigint_handler)==SIG_ERR)
@@ -21,6 +21,27 @@ if (signal(SIGINT,sigint_handler)==SIG_ERR)
   exit(1);
 
 }
+
+
+if (signal(SIGTSTP,sigtstp_handler)==SIG_ERR)
+
+{
+
+  perror("signal");
+  exit(1);
+
+}
+
+
+if (signal(SIGQUIT,sigquit_handler)==SIG_ERR)
+
+{
+
+  perror("signal");
+  exit(1);
+
+}
+
 
 printf("enter a string ;\n");
 
@@ -37,11 +58,19 @@ return 0;
 
 void sigint_handler( int sig)
 {
-printf ("jangan kacau saya!\n");
+printf ("This is special signal for signal interrupt\n");
 }
 
 void sigtstp_handler( int sign)
 {
-printf ("jangan kacau saya!\n");
+printf ("This is special signal handler for signal stop!\n");
 }
+
+void sigquit_handler( int sigq)
+{
+printf("This is special signal handler for signal quit!\n");
+}
+
+
+
 
